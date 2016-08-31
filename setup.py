78fac569b1e0358d890e6b83a270a6b0c2564b9e
Taurus Olson <taurusolson@gmail.com>
2016-08-31 11:48:31 +0200

    Create the fnapy package
    * Add tox and setup

---
+++ setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016  <>
+#
+# Distributed under terms of the MIT license.
+
+
+from setuptools import setup, find_packages
+
+requirements = [
+    'requests',
+    'xmltodict',
+    'lxml',
+    'bs4'
+]
+
+setup(
+    name='fnapy',
+    author='Taurus Olson',
+    author_email='taurusolson@gmail.com',
+    version='0.1',
+    description='A Python API for FNAC WebServices',
+    keywords=['api', 'fnac', 'python', 'webservices'],
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=True,
+    license='MIT',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
