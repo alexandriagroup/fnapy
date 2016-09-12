40888b093e689ea9a24c42bbfc06306885c4497a
Taurus Olson <taurusolson@gmail.com>
2016-09-12 15:53:31 +0200

    Use pbr in the setup

--- setup.py
+++ setup.py
@@ -16,23 +16,14 @@ requirements = [
     'bs4'
 ]
 
+setup_requirements = [
+    'pbr>=1.9', 'setuptools>=17.1'
+]
+
 setup(
-    name='fnapy',
-    author='Taurus Olson',
-    author_email='taurusolson@gmail.com',
     version='0.1',
-    description='A Python API for FNAC WebServices',
-    keywords=['api', 'fnac', 'python', 'webservices'],
-    packages=find_packages(),
     install_requires=requirements,
+    setup_requires=setup_requirements,
+    pbr=True,
     zip_safe=True,
-    license='MIT',
-    classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License'
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
 )
