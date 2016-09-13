a087034ec380cf25f9c4b46bab8644679772ba71
Taurus Olson <taurusolson@gmail.com>
2016-09-13 14:47:33 +0200

    Improve the versioning with the bumpversion script
    * Add scripts/bumpversion.py

--- setup.py
+++ setup.py
@@ -7,7 +7,23 @@
 # Distributed under terms of the MIT license.
 
 
+import re
+import os.path as op
+
+
+from codecs import open
 from setuptools import setup, find_packages
+
+
+def read(fname):
+    ''' Return the file content. '''
+    here = op.abspath(op.dirname(__file__))
+    with open(op.join(here, fname), 'r', 'utf-8') as fd:
+        return fd.read()
+
+readme = read('README.rst')
+changelog = read('CHANGELOG.rst').replace('.. :changelog:', '')
+
 
 install_requirements = [
     'requests',
@@ -16,11 +32,20 @@ install_requirements = [
     'bs4'
 ]
 
+version = ''
+version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                    read(op.join('fnapy', '__init__.py')),
+                    re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
 setup(
     name='fnapy',
     author='Taurus Olson',
     author_email='taurusolson@gmail.com',
-    version='0.1',
+    version=version,
     description='A Python API for FNAC WebServices',
     keywords=['api', 'fnac', 'python', 'webservices'],
     packages=find_packages(),
