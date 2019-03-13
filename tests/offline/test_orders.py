c344f00fd441a179b3769f50fa3327f156866ed5
Taurus Olson <taurusolson@gmail.com>
2019-03-13 10:51:30 +0100

    Add tests for the tracking number

--- tests/offline/test_orders.py
+++ tests/offline/test_orders.py
@@ -25,3 +25,17 @@ def test_update_orders(monkeypatch, fake_manager):
         fake_manager.update_orders(order_id="57BEAFDA828A8",
                                    order_update_action='accept_order',
                                    actions=[action1, action2])
+
+
+def test_update_orders_with_tracking_number(monkeypatch, fake_manager):
+    context = create_context_for_requests(monkeypatch, fake_manager,
+                                          'update_orders_with_tracking_number',
+                                          'orders_update')
+    with context:
+        action1 = {"order_detail_id": 1, "action": "Shipped",
+                   "tracking_number": "1234", "tracking_company": "Track Inc."}
+        action2 = {"order_detail_id": 2, "action": "Shipped",
+                   "tracking_number": "5678", "tracking_company": "Track Inc."}
+        fake_manager.update_orders(order_id="57BEAFDA828A8",
+                                   order_update_action='update',
+                                   actions=[action1, action2])
